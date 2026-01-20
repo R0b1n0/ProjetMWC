9da7480d34383736b52d132524cefac13b6328db
Robin Hemon <r.hemon@lahorde.school>
2026-01-20 01:06:01 +0100

    Marble Scale up on grabbed

--- Assets/Scripts/Marbles/MarbleBehaviour.cs
+++ Assets/Scripts/Marbles/MarbleBehaviour.cs
@@ -1,19 +1,29 @@
+using System.Collections;
 using UnityEngine;
 
 public class MarbleBehaviour : MonoBehaviour
 {
     private Collider coll;
-    public Transform trans;
-    public Color color;
-    public int index;
-    public MarbleState state {  get; private set; }
-    public float lerpValue = 0;
-    public Material mat;
+    [HideInInspector] public Transform trans;
+    [HideInInspector] public Color color;
+    [HideInInspector] public int index;
+    [HideInInspector] public MarbleState state {  get; private set; }
+    [HideInInspector] public float lerpValue = 0;
+    [HideInInspector] public Material mat;
+
+    [Header("Animation")]
+    [SerializeField] AnimationCurve scalingCurve;
+    [SerializeField] float scaleOffset;
+    [SerializeField] float scaleSpeed;
+    private float sizeLerp;
+
+
+    private float initialScale;
+
 
     public Vector3 OnReleasePos { get; private set; }
 
     Renderer rend;
-
 
     private void Awake()
     {
@@ -22,6 +32,7 @@ public class MarbleBehaviour : MonoBehaviour
         rend = trans.GetComponent<Renderer>();
         mat = new(rend.material);
         rend.material = mat;
+        initialScale = trans.localScale.x;
     }
 
     public void Initialize(Color color, int index)
@@ -34,6 +45,7 @@ public class MarbleBehaviour : MonoBehaviour
 
     public void SetState(MarbleState newState)
     {
+        lerpValue = 0;
         //Enter
         switch (newState)
         {
@@ -60,13 +72,44 @@ public class MarbleBehaviour : MonoBehaviour
     public void OnGrabbed()
     {
         SetState(MarbleState.dragged);
-
+        StopAllCoroutines();
+        StartCoroutine(Expand());
     }
 
     public void OnRelease()
     {
         OnReleasePos = trans.position;
-        lerpValue = 0;
+        StopAllCoroutines();
+        StartCoroutine(Shrink());
+    }
+
+    private IEnumerator Expand()
+    {
+        float targetScale;
+        while (sizeLerp < 1)
+        {
+            sizeLerp += Time.deltaTime * scaleSpeed;
+            targetScale = initialScale + scaleOffset *  scalingCurve.Evaluate((sizeLerp));
+            trans.localScale = new Vector3(targetScale, targetScale, targetScale);
+            yield return null;
+        }
+
+        targetScale = initialScale + scaleOffset;
+        trans.localScale = new Vector3(targetScale, targetScale, targetScale);
+
+    }
+
+    private IEnumerator Shrink()
+    {
+        float targetScale;
+        while (sizeLerp > 0)
+        {
+            sizeLerp -= Time.deltaTime * scaleSpeed;
+            targetScale = initialScale + scaleOffset * scalingCurve.Evaluate((sizeLerp));
+            trans.localScale = new Vector3(targetScale, targetScale, targetScale);
+            yield return null;
+        }
+        trans.localScale = new Vector3(initialScale, initialScale, initialScale);
     }
 }
 
