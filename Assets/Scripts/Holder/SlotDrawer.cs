839b7a1b2e2f56c63d2eee58bea2c1cefc045b8e
Robin Hemon <r.hemon@lahorde.school>
2026-04-02 11:47:17 +0200

    Android build friendly code

--- Assets/Scripts/Holder/SlotDrawer.cs
+++ Assets/Scripts/Holder/SlotDrawer.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.WSA;
 
 [RequireComponent(typeof(RectTransform))]
 public class SlotDrawer : MonoBehaviour
